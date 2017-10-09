ebc2afb4b6588a83fe01268781c8484b5f296492
Tushar Pokle <tushar.pokle@gmail.com>
2017-10-09 23:43:03 +1100

    refactor

--- README.md
+++ README.md
@@ -1,6 +1,19 @@
 
 # purescript-node-http Hello World
 
+My 'Hello World' trial of https://github.com/purescript-node/purescript-node-http
+
     bower i
     pulp build
+
+    # Start single instance
+    pulp run
+
+    # Or start a cluster with pm2
     pm2 start app.js -i 4 --name "app"
+
+# Learnings
+
+- purescript-node-http is a very low level library - wrapping nodejs' OO mutating api
+- This makes it quite unsafe to use. 
+- I'd probably look for something else for writing apps.
--- src/Main.purs
+++ src/Main.purs
@@ -4,27 +4,28 @@ import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
 import Data.Maybe (Maybe(..))
 import Node.Encoding (Encoding(..))
-import Node.HTTP (HTTP, ListenOptions, Request, Response, createServer, listen, responseAsStream, setStatusCode, setStatusMessage)
+import Node.HTTP (HTTP, ListenOptions, Request, Response, createServer, listen, responseAsStream, setStatusCode)
 import Node.Stream (end, writeString)
-import Prelude (Unit, bind, discard, pure, unit)
+import Prelude (Unit, bind, discard, pure, show, unit, (<>))
 
+respond :: forall eff. Response -> Int -> String -> Eff (http :: HTTP | eff) Unit
+respond res status str =
+  do
+    let outputStream = responseAsStream res
+    setStatusCode res status
+    _ <- writeString outputStream UTF8 str (pure unit)
+    end outputStream (pure unit)
 
 app :: forall eff. Request -> Response -> Eff (http :: HTTP, console :: CONSOLE | eff) Unit
 app req res = do
-  let outputStream = responseAsStream res
-  setStatusCode res 200
-  setStatusMessage res "OK dokey"
-  _ <- writeString outputStream UTF8 "Yay!" (pure unit)
-  end outputStream (pure unit)
-  log "."
-  pure unit
-
+  respond res 200 "Hello Sailor" 
+  log "Responded."
 
 listenOptions :: ListenOptions
-listenOptions = { hostname: "0.0.0.0", port: 8000, backlog: Nothing }
+listenOptions = { hostname: "127.0.0.1", port: 8000, backlog: Nothing }
 
--- main :: forall e. Eff (console :: CONSOLE | e) Unit
 main :: forall e. Eff ( http :: HTTP , console :: CONSOLE | e ) Unit
 main = do
-  s <- createServer app
-  listen s listenOptions (log "🔋 ready")
+  let onReady = log ("🔋 ready at http://" <> listenOptions.hostname <> ":" <> (show listenOptions.port))
+  server <- createServer app
+  listen server listenOptions onReady
